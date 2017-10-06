931bf091cc747ebd4637a0c989767d4792e1dab2
Kristian Monsen Haug <kristian.haug95@gmail.com>
2017-10-06 11:13:43 +0200

    Change configuration to fit the new version

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,17 @@
+c.url.searchengines = {'DEFAULT': 'https://duckduckgo.com/?q={}',
+        'r': 'https://reddit.com/r/{}',
+        'g': 'https://google.com/search?q={}',
+        'so': 'https://stackoverflow.com/search?q={}',
+        'w': 'https://en.wikipedia.org/w/index.php?search={}'
+        }
+
+config.bind('d', 'scroll-page 0 0.5')
+config.bind('u', 'scroll-page 0 -0.5')
+
+config.bind('x', 'tab-close')
+config.bind('X', 'tab-close -o')
+
+config.bind('W', 'tab-detach')
+config.bind('K', 'tab-next')
+config.bind('J', 'tab-prev')
+
